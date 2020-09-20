11045c4d77ddb8119dc0a14cd69bf69b1bae6b15
Gustavo Cotta <gmcotta34@gmail.com>
2020-09-20 15:20:56 -0300

    initial configuration, setting global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 'border-box';
+  }
+
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+    height: 100%;
+    width: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  :root {
+    --color-white: #fff;
+    --color-light: #fffcf7;
+    --color-black: #000;
+    --color-dark: #2b2d42;
+    --color-primary: #6186c4;
+    --color-gray-light: #eee;
+    --color-gray-dark: #8a8d8b;
+    --color-green: #a3c299;
+    --color-red: #bf3b26;
+  }
+`;
